bd58dde72eddf034d73d4dd50e83c965252b916f
Brian Risk <brian@geneffects.com>
2022-09-26 14:12:28 -0400

    updating __init__.py for imports

--- simphile/__init__.py
+++ simphile/__init__.py
@@ -1,2 +1,20 @@
-from . import textsimilarity, sets, naive_bayes
-__all__ = [textsimilarity, sets, naive_bayes]
+from . import textsimilarity, sets
+from naive_bayes import NaiveBayes
+from textsimilarity import compression_similarity, CompressionSimilarity
+from textsimilarity import euclidian_similarity, EuclidianSimilarity
+from textsimilarity import jaccard_similarity, JaccardSimilarity
+from textsimilarity.text_processor import TextProcessor
+from textsimilarity import text_utils
+
+__all__ = [
+    "sets",
+    "NaiveBayes",
+    "compression_similarity",
+    "CompressionSimilarity",
+    "euclidian_similarity",
+    "EuclidianSimilarity",
+    "jaccard_similarity",
+    "JaccardSimilarity",
+    "TextProcessor",
+    "text_utils"
+]
